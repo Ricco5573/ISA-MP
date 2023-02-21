a9e5006e59d2409eee2d8b1f9dc8447a5f5b43ec
Ricco5573 <ricardoboekestijn@gmail.com>
2023-02-21 12:48:43 +0100

    Added basic movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,23 +7,34 @@ using UnityEngine;
 
 public class PlayerMovement : NetworkBehaviour
 {
+
+    private int maxSpeed;
+    private int speed = 50;
+    private Rigidbody rb;
+    private int health = 100;
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = this.gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.LeftShift) && speed < 100)
+        {
+            speed++;
+        }
+        else if(Input.GetKey(KeyCode.LeftControl) && speed > 0)
+        {
+            speed--;
+        }
+
+        rb.velocity.Set(speed, 0, 0);
+        Debug.Log(rb.velocity);
+        Debug.Log(speed);
     }
 
-
-
-    public void Movement(Vector2 direction)
-    {
-       
-    }
     [ClientRpc]
     void MoveClientRpc(int value)
     {
