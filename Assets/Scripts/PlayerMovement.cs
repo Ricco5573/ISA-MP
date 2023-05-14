a68449a6a931b24a9f088e257932a47d667a50c8
Ricco5573 <ricardoboekestijn@gmail.com>
2023-05-14 14:10:18 +0200

    Working on flying functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using Unity.Netcode;
 using UnityEditor.PackageManager;
 using UnityEngine;
@@ -7,43 +8,74 @@ using UnityEngine;
 
 public class PlayerMovement : NetworkBehaviour
 {
-
-    private int maxSpeed;
-    private int speed = 50;
+    public float rollSpeed = 50f;         // Speed of roll rotation
+    public float bankSpeed = 20f;          // Speed of bank rotation
+    public float turnSpeed = 75f;         // Speed of turning
+    public float acceleration = 2000f;        // Acceleration rate
+    public float deceleration = 30f;        // Deceleration rate
+    public float maxSpeed = 2000f;           // Maximum speed
+    public float minSpeed = 5f;            // Minimum speed
+    public float altitudeLossRate = 0.5f;  // Rate of altitude loss
     private Rigidbody rb;
-    private int health = 100;
-    // Start is called before the first frame update
-    void Start()
+    private float currentSpeed = 0f;       // Current speed of the player
+    private float currentAltitude = 0f;    // Current altitude of the player
+
+
+
+    private void Start()
     {
-        rb = this.gameObject.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKey(KeyCode.LeftShift) && speed < 100)
-        {
-            speed++;
-        }
-        else if(Input.GetKey(KeyCode.LeftControl) && speed > 0)
-        {
-            speed--;
-        }
+        acceleration = 2000f;
+        maxSpeed = 2000f;
+        // Get input for rolling and banking
+        float rollInput = Input.GetAxis("Roll");
+        float bankInput = Input.GetAxis("Bank");
 
-        rb.velocity.Set(speed, 0, 0);
-        Debug.Log(rb.velocity);
-        Debug.Log(speed);
-    }
+        // Calculate roll and bank rotation
+        float roll = rollInput * rollSpeed * Time.deltaTime;
+        float bank = bankInput * bankSpeed * Time.deltaTime;
 
-    [ClientRpc]
-    void MoveClientRpc(int value)
-    {
+        // Apply roll and bank rotations
+        transform.Rotate(Vector3.forward, -roll, Space.Self);
+        transform.Rotate(Vector3.right, bank, Space.Self);
 
-    }
+        // Get input for turning    
+        float turnInput = Input.GetAxis("Vertical");
 
-    [ServerRpc]
-    void MoveServerRpc(int value)
-    {
+        // Calculate turn rotation
+        float turn = turnInput * turnSpeed * Time.deltaTime;
 
+        // Apply turn rotation
+        transform.Rotate(Vector3.up, turn, Space.Self);
+
+        // Get input for accelerating and decelerating
+        float throttleInput = Input.GetAxis("Throttle");
+
+
+        Debug.Log(maxSpeed);      // Calculate speed change
+        float speedChange = throttleInput * acceleration;
+    
+        // Apply speed change
+        currentSpeed = Mathf.Clamp(currentSpeed + speedChange, minSpeed, maxSpeed);
+
+        // Move the player forward based on the current speed
+        rb.AddForceAtPosition( this.gameObject.transform.forward * currentSpeed, gameObject.transform.position);
+
+        // Check if the player is not moving fast enough
+         if (currentSpeed < minSpeed)
+         {
+             // Calculate altitude loss
+             float altitudeLoss = altitudeLossRate * Time.deltaTime;
+
+             // Decrease the altitude
+             currentAltitude -= altitudeLoss;
+
+             // Move the player downwards
+             transform.Translate(Vector3.down * altitudeLoss);
+         }
     }
 }
+
